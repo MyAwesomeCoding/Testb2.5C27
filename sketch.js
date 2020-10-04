e847c81f22612a92767f47db8fb7b3c80182e03f
MyAwesomCoding <priyaa.sekhar@gmail.com>
2020-10-04 21:58:09 +0530

    modified to create a log and constraint

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var constrainedLog;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -18,6 +19,7 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
+    constrainedLog = new Log(230,180,80,PI/2);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -35,9 +37,16 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    var options = {
+	    bodyA: bird.body,
+	    bodyB: constrainedLog.body,
+	    stiffness: 0.04,
+	    length:10
 
-}
-
+      }
+    var chain = Constraint.create(options);
+	World.add(world,chain);
+    }
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
@@ -61,4 +70,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    constrainedLog.display();
+    line(bird.body.position.x,bird.body.position.y,constrainedLog.body.position.x,constrainedLog.body.position.y);
 }
